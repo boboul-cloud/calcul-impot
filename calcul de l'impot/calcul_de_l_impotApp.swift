2789f59c9f9b2b2b718d0b29170bda081f48a5a7
BudgetApp <bob.oulhen@gmail.com>
2026-04-12 19:27:37 +0200

    Initial Commit

---
+++ calcul de l'impot/calcul_de_l_impotApp.swift
@@ -0,0 +1,17 @@
+//
+//  calcul_de_l_impotApp.swift
+//  calcul de l'impot
+//
+//  Created by Robert Oulhen on 12/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct calcul_de_l_impotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
